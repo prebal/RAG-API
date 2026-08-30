35edfa0980f4f89d2f3f196f02a057c7ec517f5b
gask09_Desktop <balaz.premek1@seznam.cz>
2026-08-31 00:30:29 +0200

    added FileNotFoundError for some cases where file has been deleted first

--- app/services/storage_service.py
+++ app/services/storage_service.py
@@ -12,9 +12,7 @@ CHUNKSIZE = 1024 * 1024
 class StorageService:
     async def save_document_storage(self, uploaded_file: UploadFile) -> str:
         file_format = str(uploaded_file.filename).split(".")[-1].lower()
-        full_destination = (
-            f"{get_settings().storage}/{uuid.uuid4()}.{file_format}"
-        )
+        full_destination = f"{get_settings().storage}/{uuid.uuid4()}.{file_format}"
         async with aiofiles.open(full_destination, "wb") as saved_file:
             while chunk := await uploaded_file.read(CHUNKSIZE):
                 await saved_file.write(chunk)
@@ -22,4 +20,7 @@ class StorageService:
         return full_destination
 
     def remove_document_storage(self, file_to_delete: str) -> None:
-        os.remove(file_to_delete)
+        try:
+            os.remove(file_to_delete)
+        except FileNotFoundError:
+            pass
